3cf31b92f1a997c35ee4731c7fd370dcdf974482
emanuellMarinho <joaoemanuelribeiromarinho@gmail.com>
2023-07-07 22:24:27 -0300

    ATIVIDADE PRÁTICA - UNINTER

--- OOP/src/application/Cofrinho.java
+++ OOP/src/application/Cofrinho.java
@@ -2,14 +2,14 @@ package application;
 
 import java.util.ArrayList;
 
-public class Cofrinho{
+public class Cofrinho {
 	private ArrayList<String> listMoedas = new ArrayList<>();
-	
+
 	// Adiciona moedas ao Array
 	public void adicionarMoeda(String moeda) {
 		listMoedas.add(moeda);
 	}
-	
+
 	// Remove moedas do Array
 	public void removerMoeda(String moeda) {
 		listMoedas.remove(moeda);
